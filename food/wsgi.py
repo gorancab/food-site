0488f6a48fd96619e3a23e0638064ca27b24faf1
g.cabarkapa <g.cabarkapa@a1.rs>
2023-03-25 22:04:25 +0100

    New commit!

--- food/wsgi.py
+++ food/wsgi.py
@@ -1,13 +1,9 @@
+import logging
+import sys
+
 from food import flask_app
 
-if __name__ == '__main__':
-    # Run the app locally for testing
-    flask_app.run(host='0.0.0.0', port=5000)
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, '/var/www/gcab_pythonanywhere_com')
 
-# Use gunicorn as the production WSGI server
-else:
-    import logging
-    import sys
-    logging.basicConfig(stream=sys.stderr)
-    sys.path.insert(0, '/var/www/gcab_pythonanywhere_com')
-    from flask_app import flask_app as application
+application = flask_app
